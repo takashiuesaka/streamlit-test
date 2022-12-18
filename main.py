0c728a97ebcd351f7b5657421a2f3c4ccb701cb7
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-12-18 09:45:52 +0000

    request to .auth/me

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import streamlit as st
-from streamlit.web.server.websocket_headers import _get_websocket_headers
-
-headers = _get_websocket_headers()
-access_token = headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
+import requests
 
 st.title('Easy Auth Test2')
 
-st.text('X-MS-CLIENT-PRINCIPAL-NAME')
+api_url = ".auth/me"
+response = requests.get(api_url)
 
-if access_token is not None:
-    st.text(access_token)
-else:
-    st.text('X-MS-CLIENT-PRINCIPAL-NAME is not in header.')
+st.text(response.json())
