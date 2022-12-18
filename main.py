39b4eb2277327b8d2fce56f95467b7ba8ce37024
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-12-18 09:09:50 +0000

    Easy Auth Test

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from streamlit.web.server.websocket_headers import _get_websocket_headers
 
-st.title('test5')
+headers = _get_websocket_headers()
+access_token = headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
+
+st.title('Easy Auth Test')
+
+if access_token is not None:
+    st.text('X-MS-CLIENT-PRINCIPAL-NAME')
+    st.text(access_token)
