9e9a7de25dcd4ad7246ffcd89754dd7b315505c4
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-12-19 00:29:09 +0000

    hello

--- main.py
+++ main.py
@@ -2,13 +2,5 @@ import streamlit as st
 import requests
 import socket
 
-st.title('Easy Auth Test3')
+st.title('Hello!')
 
-host = socket.gethostname()
-st.text('hostname: ' + host)
-
-
-api_url = "https://msue-python-streamlit.azurewebsites.net/.auth/me"
-response = requests.get(api_url)
-
-st.text(response)
