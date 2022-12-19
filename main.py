0ce9ec165c40e363b5ff06482bce04648ef9d457
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-12-19 01:18:49 +0000

    show headers

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import streamlit as st
-import requests
-import socket
+from streamlit.scriptrunner.script_run_context import get_script_run_ctx
+from streamlit.server.server import Server
 
-st.title('Hello!')
+st.title('Hello! test session and headers')
 
+# Get headers
+session_id = get_script_run_ctx().session_id
+server = Server.get_current()
+session_info = server._get_session_info(session_id)
+if session_info.ws is None:
+    # At first page load, this is None (at least until #4099 is fixed)
+    st.markdown("Unable to get session websocket. Please refresh the page.")
+    st.stop()
+headers = session_info.ws.request.headers
+
+st.text('headers')
+st.text(headers)
