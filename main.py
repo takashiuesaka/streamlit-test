70f622308f2b6ada865edda3bfd2f2becd18aaca
Takashi Uesaka <takashiuesaka1224@gmail.com>
2022-12-19 02:06:44 +0000

    final version

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import streamlit as st
 from streamlit.web.server.websocket_headers import _get_websocket_headers
 
 
-st.title('Hello! test session and headers')
+st.title('Hello! This streamlit app is on Azure AppService.')
 
 headers = _get_websocket_headers()
-access_token = headers.get("X-Access-Token")
+principal_name = headers.get("X-Ms-Client-Principal-Name")
+principal_id = headers.get("X-Ms-Client-Principal-Id")
+access_token = headers.get("X-Ms-Token-Aad-Access-Token")
 
-st.text('headers count')
+if principal_name is not None:
+    st.markdown('X-Ms-Client-Principal-Name: ' + principal_name)
+if principal_id is not None:
+    st.markdown('X-Ms-Client-Principal-Name: ' + principal_id)
+if access_token is not None:
+    st.markdown('X-Ms-Client-Principal-Name: ' + access_token)
+
+st.header('http headers count')
 st.text(len(headers))
 
+st.header('List http headers')
 if headers is not None:
     for key, value in headers.items():
         text = 'key={}, value={}'
